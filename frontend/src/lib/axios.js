07643fc0ad36645421887fe931191f7a8ed08c05
TheCringyOne <136010239+TheCringyOne@users.noreply.github.com>
2025-05-21 14:51:43 -0600

    Update axios.js
    se modifico la instancia de axios para que se envien las solicitudes al dominio ahora que se esta en prod

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// Determinar la URL base de la API según el entorno
+const baseURL = import.meta.env.MODE === "production" 
+  ? "" // URL vacía para usar el mismo dominio
+  : "http://localhost:5000/api/v1";
+
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api/v1",
-	//en /api/v1 vamos a poner el link donde va a estar nuestro servidor, en este caso nos lo tienen que dar en el tec 
-	withCredentials: true,
+  baseURL,
+  withCredentials: true,
 });
