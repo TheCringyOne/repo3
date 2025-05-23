c140da917e1ef580243014371c9a5311be326fa9
TheCringyOne <136010239+TheCringyOne@users.noreply.github.com>
2025-05-22 21:19:23 -0600

    Update post.route.js
    borrar comentarios

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -6,6 +6,7 @@ import {
 	deletePost,
 	getPostById,
 	createComment,
+	deleteComment,
 	likePost,
 } from "../controllers/post.controller.js";
 
@@ -16,6 +17,7 @@ router.post("/create", protectRoute, createPost);
 router.delete("/delete/:id", protectRoute, deletePost);
 router.get("/:id", protectRoute, getPostById);
 router.post("/:id/comment", protectRoute, createComment);
+router.delete("/:postId/comment/:commentId", protectRoute, deleteComment);
 router.post("/:id/like", protectRoute, likePost);
 
 export default router;
